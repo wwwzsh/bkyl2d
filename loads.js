d3de582108712eef7d11439c87c0ec798002e05c
wwwzsh <40414544+wwwzsh@users.noreply.github.com>
2024-05-26 02:33:29 +0800

    Update loads.js

--- loads.js
+++ loads.js
@@ -33,7 +33,7 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			OwOPath: live2d_path + "OwO.json",
+			//OwOPath: live2d_path + "OwO.json",
 			// apiPath: "https://live2d.fghrsh.net/api/",
 			// cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
 			// cdnPath: "https://live2d-api.vercel.app/"
